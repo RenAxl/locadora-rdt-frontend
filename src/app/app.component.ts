914f46979a53afa610f0a6f35ec9f1e1a915b5b0
Renan Duarte Gomes <renandg.axl@gmail.com>
2025-12-03 15:42:29 -0300

    Bootstrap and PrimeNG configured

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  role: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'locadora-rdt-frontend';
+  
+  users: User[] = [];
+
+  ngOnInit(): void {
+    this.users = [
+      { id: 1, name: 'Renan', email: 'renan@example.com', role: 'Admin' },
+      { id: 2, name: 'Maria', email: 'maria@example.com', role: 'User' },
+      { id: 3, name: 'João', email: 'joao@example.com', role: 'User' }
+    ];
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TableModule } from 'primeng/table';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+     BrowserAnimationsModule, 
+    TableModule 
   ],
   providers: [],
   bootstrap: [AppComponent]

08f1e0fda5c918c5ef0b46f52072ef472f78fd8f
Renan Duarte Gomes <renandg.axl@gmail.com>
2025-12-19 15:32:39 -0300

    Route, button, and layout of the user input form.

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
@@ -7,21 +8,23 @@ import { TooltipModule } from 'primeng/tooltip';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UserListComponent } from './user-list/user-list.component';
-import { UsersService } from './users.service';
 import { SharedModule } from '../shared/shared.module';
+import { UserFormComponent } from './user-form/user-form.component';
 
 
 
 @NgModule({
   declarations: [
-    UserListComponent
+    UserListComponent,
+    UserFormComponent
   ],
   imports: [
     CommonModule,
+    FormsModule,
     TableModule,
-     ButtonModule,
-     TooltipModule,
-     SharedModule,
+    ButtonModule,
+    TooltipModule,
+    SharedModule,
 
     UsersRoutingModule
   ],

c10836bac68eee3f20d1da4cf2ce73059dfa6391
Renan Duarte Gomes <renandg.axl@gmail.com>
2026-01-21 20:36:37 -0300

    Layout of the Roles management page

--- src/app/roles/role-list/role-list.component.ts
+++ src/app/roles/role-list/role-list.component.ts
@@ -1,15 +1,67 @@
 import { Component, OnInit } from '@angular/core';
 
+type RoleRow = {
+  id: number;
+  name: string;
+  description: string;
+  permissionsCount: number;
+};
+
 @Component({
   selector: 'app-role-list',
   templateUrl: './role-list.component.html',
-  styleUrls: ['./role-list.component.css']
+  styleUrls: ['./role-list.component.css'],
 })
 export class RoleListComponent implements OnInit {
+  roles: RoleRow[] = [];
+  selectedRoles: RoleRow[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+     this.roles = [
+      {
+        id: 1,
+        name: 'ADMINISTRADOR',
+        description: 'Acesso total ao sistema',
+        permissionsCount: 42,
+      },
+      {
+        id: 2,
+        name: 'GERENTE',
+        description: 'Gerencia operações e relatórios',
+        permissionsCount: 28,
+      },
+      {
+        id: 3,
+        name: 'ATENDENTE',
+        description: 'Atendimento e cadastro básico',
+        permissionsCount: 14,
+      },
+      {
+        id: 4,
+        name: 'FINANCEIRO',
+        description: 'Cobranças, pagamentos e faturamento',
+        permissionsCount: 19,
+      },
+      {
+        id: 5,
+        name: 'CLIENTE',
+        description: 'Acesso somente às próprias informações',
+        permissionsCount: 6,
+      },
+      {
+        id: 6,
+        name: 'AUDITORIA',
+        description: 'Visualização e trilhas de auditoria',
+        permissionsCount: 11,
+      },
+      {
+        id: 7,
+        name: 'SUPORTE',
+        description: 'Apoio técnico e atendimento interno',
+        permissionsCount: 9,
+      },
+    ];
   }
-
 }
--- src/app/roles/roles.module.ts
+++ src/app/roles/roles.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { TooltipModule } from 'primeng/tooltip';
+
 import { RolesRoutingModule } from './roles-routing.module';
 import { RoleListComponent } from './role-list/role-list.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
-  declarations: [
-    RoleListComponent
-  ],
+  declarations: [RoleListComponent],
   imports: [
     CommonModule,
-    
-    RolesRoutingModule
-  ]
+    TableModule,
+    ButtonModule,
+    TooltipModule,
+    SharedModule,
+
+    RolesRoutingModule,
+  ],
 })
-export class RolesModule { }
+export class RolesModule {}

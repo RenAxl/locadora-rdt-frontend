fba49aa85364a9c38945f5a623c1107bfa379e15
Renan Duarte Gomes <renandg.axl@gmail.com>
2026-02-20 21:26:12 -0300

    Changing the structure of the home component

--- src/app/features/home/home-routing.module.ts
+++ src/app/features/home/home-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home.component';
+import { HomeComponent } from './pages/home.component';
+import { AuthGuard } from 'src/app/core/auth/guards/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    canActivate: [AuthGuard],
   }
 ];
 
